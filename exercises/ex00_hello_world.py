7a7d0a0fc8d501cddc8c5f9cba95e0f7b3312236
= <robertlewison@gmail.com>
2022-08-17 21:24:34 -0400

    changed the single quotes to triple quotes and put an exclamation point at the end instead of a smiley face

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,4 +1,4 @@
-"i am so excited to be coding again. this class seems AWESOME :)"
+"""i am so excited to be coding again. this class seems AWESOME :)!"""
 __author__ = "730571332"
 
 print("Hello, world.")
