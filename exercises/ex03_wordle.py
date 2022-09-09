1f44c8424eee83af269fecf48ef319f87eb23be1
= <robertlewison@gmail.com>
2022-09-09 01:24:16 -0400

    fixed capitalization/punctuation "issues"

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,19 +1,23 @@
-"""a structured wordle."""
+"""A structured wordle."""
 __author__ = "730571332"
 
 
 def contains_char(word: str, letter: str) -> bool:
-    """this function will check to see if a letter is in a word"""
+
+    """This function will check to see if a letter is in a word."""
+    
     assert len(letter) == 1
     i = len(word)
     while i > 0:
-        if word[i-1] == letter:
+        if word[i - 1] == letter:
             return True
         i -= 1
     return False
 
 def emojified(guess: str, secret: str) -> str:
-    """this function will return a string of emojis giving the user info on their guess"""
+
+    """This function will return a string of emojis giving the user info on their guess."""
+
     wBox: str = "\U00002B1C"
     gBox: str = "\U0001F7E9"
     yBox: str = "\U0001F7E8"
@@ -31,17 +35,21 @@ def emojified(guess: str, secret: str) -> str:
     return key
 
 def input_guess(expL: int) -> str:
-    """getting an input for the expected length from the user"""
+
+    """Getting an input for the expected length from the user."""
+
     userGuess: str = input(f"Enter a {expL} character word: ")
     while len(userGuess) != expL:
         userGuess = input(f"That wasn't {expL} chars! Try again: ")
     return userGuess
 
 def main() -> None:
-    """the entrypoint of the program and main game loop"""
+
+    """The entrypoint of the program and main game loop."""
+
     secretWord = "coders"
     i = 1
-    """using i < 7 because user only gets 6 votes, not dependent on word length"""
+    """Using i < 7 because user only gets 6 votes, not dependent on word length."""
     while i < 7:
         print(f"~ turn {i}/6 ~")
         guessedWord = input_guess(len(secretWord))
@@ -52,6 +60,6 @@ def main() -> None:
         i += 1
     print("x/6 - sorry, try again tomorrow!")
 
-"""mystery code lol"""
+"""Mystery code lol."""
 if __name__ == "__main__":
     main()
