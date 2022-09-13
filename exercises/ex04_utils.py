cd4bf977d0b87aa9afb584d4af4a319e768c11e1
= <robertlewison@gmail.com>
2022-09-13 16:37:56 -0400

    finished ex04 utils!

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,32 @@
+"""This code will do something with lists to my understanding."""
+__author__ = "730571332"
+
+def all(allInts: list, givenInt: int) -> bool:
+    """Will determine if every component of the list matches the givenInt."""
+    i = 0
+    while i < len(allInts):
+        if allInts[i] == givenInt:
+            i += 1
+        else:
+            return False
+    return True
+
+def max(maxInts: list) -> int:
+    """Returns the largest number in a list."""
+    i = 0
+    while i < (len(maxInts) - 1):
+        if maxInts[i + 1] > maxInts[i]:
+            biggestNumber = maxInts[i + 1]
+        i += 1
+    return biggestNumber
+
+def is_equal(firstList: list, secondList: list) -> bool:
+    """Checks whether or not the lists share deep equality."""
+    i = 0
+    assert len(firstList) == len(secondList)
+    while i < len(firstList):
+        if firstList[i] == secondList[i]:
+            i += 1
+        else:
+            return False
+    return True
