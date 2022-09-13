01cbea4fab2003cac1316b9a3c248ae12698a3c1
= <robertlewison@gmail.com>
2022-09-13 16:48:44 -0400

    updated length between definitions

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,6 +1,7 @@
 """This code will do something with lists to my understanding."""
 __author__ = "730571332"
 
+
 def all(allInts: list, givenInt: int) -> bool:
     """Will determine if every component of the list matches the givenInt."""
     i = 0
@@ -11,6 +12,7 @@ def all(allInts: list, givenInt: int) -> bool:
             return False
     return True
 
+
 def max(maxInts: list) -> int:
     """Returns the largest number in a list."""
     i = 0
@@ -20,10 +22,12 @@ def max(maxInts: list) -> int:
         i += 1
     return biggestNumber
 
+
 def is_equal(firstList: list, secondList: list) -> bool:
     """Checks whether or not the lists share deep equality."""
     i = 0
-    assert len(firstList) == len(secondList)
+    if len(firstList) != len(secondList):
+        return False
     while i < len(firstList):
         if firstList[i] == secondList[i]:
             i += 1
