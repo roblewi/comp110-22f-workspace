0ed53a3dea726548dd0fda38501d704c5253fb6b
robert :) <robertlewison@gmail.com>
2022-10-12 13:21:25 -0400

    csv/jupyter lesson follow-alongs

---
+++ lessons/data_utils.py
@@ -0,0 +1,46 @@
+"""Some helpful utility functions for working with CSV files."""
+
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a CSV into a 'table'."""
+    result: list[dict[str, str]] = []
+
+    # Open the file
+    file_handle = open(filename, "r", encoding="utf8")
+    
+    # Read the file
+    csv_reader = DictReader(file_handle)
+
+    # Append each row of csv_reader to result
+    for row in csv_reader:
+        result.append(row)
+    
+    
+    # Close the file
+    file_handle.close()    
+
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column."""
+    result: list[str] = []
+
+    for row in table:
+        item: str = row[column]
+        result.append(item)
+
+    return result
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+
+    return result
